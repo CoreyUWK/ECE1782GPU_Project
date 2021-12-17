23740fe29a4f56e9f13d507ed4f0e3756b84d82e
fabian <36959980+ftorres16@users.noreply.github.com>
2021-12-17 13:52:44 -0500

    cnn parallel reuse max pool2d

--- testbench/cnnParallel.cu
+++ testbench/cnnParallel.cu
@@ -36,6 +36,7 @@ then write out to ouput (inline or not)
 
 */
 #include "../src/cnn_weights.h"
+#include "../src/layers/max_pool_2d.cu"
 #include "../src/utils.cu"
 #include <algorithm>
 #include <math.h>
@@ -57,56 +58,9 @@ then write out to ouput (inline or not)
 
 #define NUM_STREAM 64 + 64
 
-// MAXPOOL Config
-#define PAD_VALUE -INFINITY
-#define MAX_TOL 1e-3
-#define POOL_SIZE 2
-#define STRIDE POOL_SIZE
-
 // Currently a thread per pooling, but thread no reading coalesed
 // could read coalesed by copying to shared memory and then reorder in shared
 // memory linearly
-__global__ void max_pool_2d(float *in, int in_rows, int in_cols, float *out,
-                            int out_rows, int out_cols) {
-    unsigned int o_col = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int o_row = blockDim.y * blockIdx.y + threadIdx.y;
-
-    float out_element = PAD_VALUE;
-    float current_element;
-    unsigned int addr;
-
-    if (o_col >= out_cols || o_row >= out_rows) {
-        return;
-    }
-
-    // Implement padding=same from tensorflow
-    int px_pre = (in_cols % STRIDE == 0) ? max(POOL_SIZE - STRIDE, 0)
-                                         : max(POOL_SIZE - in_cols % STRIDE, 0);
-    int py_pre = (in_rows % STRIDE == 0) ? max(POOL_SIZE - STRIDE, 0)
-                                         : max(POOL_SIZE - in_rows % STRIDE, 0);
-    px_pre /= 2;
-    py_pre /= 2;
-
-    int i_y_min = o_row * STRIDE - py_pre;
-    int i_x_min = o_col * STRIDE - px_pre;
-
-    for (int i_col = i_x_min; i_col < i_x_min + POOL_SIZE; i_col++) {
-        for (int i_row = i_y_min; i_row < i_y_min + POOL_SIZE; i_row++) {
-            addr = i_row * in_cols + i_col;
-
-            current_element =
-                (i_col >= 0 && i_col < in_cols && i_row >= 0 && i_row < in_rows)
-                    ? in[addr]
-                    : PAD_VALUE;
-
-            if (current_element > out_element)
-                out_element = current_element;
-        }
-    }
-
-    addr = o_row * out_cols + o_col;
-    out[addr] = out_element;
-}
 
 __global__ void device_CNN(int in_cols, int in_rows, float *inCh, float *outCh,
                            float b, float *filter, int filterSize,
